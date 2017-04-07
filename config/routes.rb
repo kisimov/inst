f83747cd97faddc344d4b4230fadbb4214fe6479
Simon Kisimov <skisimov@gmail.com>
2017-04-07 12:07:55 +0300

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'posts#index'
+  resources :posts
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root to: "home#index"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
